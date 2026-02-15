c2c799c626d27b98aed09b0b38f99ecf681943fe
AloisioFernandes <aloisio.fernandes@outlook.com>
2026-02-15 13:51:52 -0300

    Controller comentar pergunta.

--- src/domain/forum/application/use-cases/comment-on-question.ts
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -1,9 +1,10 @@
 import { left, right, type Either } from "@/core/either";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Injectable } from "@nestjs/common";
 import { ResourceNotFoundError } from "../../../../core/errors/errors/resource-not-found-error";
 import { QuestionComment } from "../../enterprise/entities/question-comment";
-import type { QuestionCommentsRepository } from "../repositories/question-comments-repository";
-import type { QuestionsRepository } from "../repositories/questions-repository";
+import { QuestionCommentsRepository } from "../repositories/question-comments-repository";
+import { QuestionsRepository } from "../repositories/questions-repository";
 
 interface CommentOnQuestionUseCaseRequest {
   authorId: string;
@@ -18,6 +19,7 @@ type CommentOnQuestionUseCaseResponse = Either<
   }
 >;
 
+@Injectable()
 export class CommentOnQuestionUseCase {
   constructor(
     private questionsRepository: QuestionsRepository,

4e45f4534ff1fd09cfb15494f5aa577b9316f673
AloisioFernandes <aloisio.fernandes@outlook.com>
2026-03-19 21:12:41 -0300

    Registrando eventos de domínio.

--- src/domain/notification/application/subscribers/on-question-best-answer-chosen.ts
+++ src/domain/notification/application/subscribers/on-question-best-answer-chosen.ts
@@ -1,9 +1,11 @@
 import { DomainEvents } from "@/core/events/domain-events";
-import type { EventHandler } from "@/core/events/event-handler";
-import type { AnswersRepository } from "@/domain/forum/application/repositories/answers-repository";
+import { EventHandler } from "@/core/events/event-handler";
+import { AnswersRepository } from "@/domain/forum/application/repositories/answers-repository";
 import { QuestionBestAnswerChosenEvent } from "@/domain/forum/enterprise/events/question-best-answer-chosen";
-import type { SendNotificationUseCase } from "../use-cases/send-notification";
+import { Injectable } from "@nestjs/common";
+import { SendNotificationUseCase } from "../use-cases/send-notification";
 
+@Injectable()
 export class OnQuestionBestAnswerChosen implements EventHandler {
   constructor(
     private answersRepository: AnswersRepository,
